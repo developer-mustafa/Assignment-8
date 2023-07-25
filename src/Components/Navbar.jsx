4e9da4c5f0a60328ca1fc0344485f369eb3834f3
developer-mustafa <mustafarahman792@gmail.com>
2023-07-26 02:14:30 +0600

    added button and fix

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,58 @@
 // components/Navbar.js
 
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen((prev) => !prev);
+  };
+
   return (
     <nav className="bg-blue-500 p-4">
-      <div className="container mx-auto">
-        <ul className="flex space-x-4">
+      <div className="container mx-auto flex items-center justify-between">
+        {/* Logo */}
+        <div className="flex items-center">
+          <div className="mr-2">
+            <img
+              src="https://cdn.iconscout.com/icon/premium/png-512-thumb/expense-2038073-1722975.png?f=avif&w=256"
+              alt="Logo"
+              className="w-8 h-8 bg-white"
+            />
+          </div>
+          <div className="font-semibold text-white">Income-Expense-Tracker</div>
+        </div>
+
+        {/* Hamburger Icon */}
+        <div
+          className="block lg:hidden cursor-pointer"
+          onClick={toggleMobileMenu}
+        >
+          <div
+            className={`w-6 h-1 bg-white my-1 ${
+              isMobileMenuOpen ? "rotate-45 -translate-y-1.5" : ""
+            }`}
+          ></div>
+          <div
+            className={`w-6 h-1 bg-white my-1 ${
+              isMobileMenuOpen ? "opacity-0" : ""
+            }`}
+          ></div>
+          <div
+            className={`w-6 h-1 bg-white my-1 ${
+              isMobileMenuOpen ? "-rotate-45 translate-y-1.5" : ""
+            }`}
+          ></div>
+        </div>
+
+        {/* Mobile Menu */}
+        <ul
+          className={`lg:flex space-x-4 ${
+            isMobileMenuOpen ? "flex flex-col lg:flex-row" : "hidden"
+          }`}
+        >
           <li>
             <Link to="/" className="text-white font-medium hover:text-blue-200">
               Home
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,14 +3,14 @@
 const Home = () => {
   return (
     <div className="p-4">
-      <h2 className="text-2xl font-bold">
+      <h2 className="text-2xl font-bold text-center">
         Welcome to the Income-Expense Tracker
       </h2>
-      <p className="mt-4">
+      <p className="mt-4 text-center">
         This is a simple income-expense tracker application built with React and
         Tailwind CSS.
       </p>
-      <p>
+      <p className="text-center">
         developed by Mustafa Rahman
       </p>
     </div>
